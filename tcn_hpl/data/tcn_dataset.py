ccbdaca2884af04830b08447b1957c990b228a97
Paul Tunison <paul.tunison@kitware.com>
2024-10-21 12:58:11 -0400

    Revision with major content for new TCN dataset class
    Includes offline and online data loading methods for different use cases
    to invoke as appropriate.

--- tcn_hpl/data/tcn_dataset.py
+++ tcn_hpl/data/tcn_dataset.py
@@ -1,12 +1,28 @@
+import logging
+from typing import Callable
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Sequence
+from typing import Set
+from typing import Tuple
+
+import kwcoco
+import numpy as np
+import numpy.typing as npt
 import torch
 from torch.utils.data import Dataset
-import kwcoco
 from tqdm import tqdm
-import logging
-import tcn_hpl.utils.utils as utils
 
-logger = logging.getLogger()
-logging.basicConfig(level=logging.INFO)
+from tcn_hpl.data.vectorize import (
+    FrameData,
+    FrameObjectDetections,
+    FramePoses,
+    vectorize,
+)
+
+
+logger = logging.getLogger(__name__)
 
 
 def standarize_online_inputs(inputs):
@@ -56,48 +72,284 @@ def define_tcn_vector(inputs):
 
 
 class TCNDataset(Dataset):
-    def __init__(self, kwcoco_path: str, sample_rate: int, window_size: int):
+    """
+    TCN Activity Classifier dataset.
+
+    This dataset performs common initialization and indexing of windowed data.
+
+    The mask return from the get-item method is being honored for backwards
+    compatibility, but will only ever contain 1's as there should no longer any
+    video overlap possible in returned windows.
+
+    During "online" mode, which means data is populated into this dataset via
+    the `load_data_online()` method, the truth, source video ID and source
+    frame ID returns from get-item are undefined.
+
+    Arguments:
+        window_size:
+            The size of the sliding window used to collect inputs from either a
+            real-time or offline source.
+        feature_version:
+            Integer version ID of the feature vector to generate.
+        transform:
+            Optional feature vector transformation/augmentation function.
+    """
+
+    def __init__(
+        self,
+        window_size: int,
+        feature_version: int,
+        transform: Optional[Callable] = None,
+    ):
+        self.window_size = window_size
+        self.feature_version = feature_version
+        self.transform = transform
+
+        # For offline mode, pre-cut videos into clips according to window
+        # size for easy batching.
+        # For online more, expect only one window to be set at a time via the
+        # `load_data_online` method.
+        # Content to be indexed into during __getitem__.
+        self._window_data: List[List[FrameData]] = []
+        self._window_truth: List[List[int]] = []
+        # Per-window, which source video ID it is associated with. For online
+        # mode, the value is undefined.
+        self._window_vid: List[int] = []
+        # COCO Image IDs per-frame per-window.
+        self._window_gids: List[List[int]] = []
+
+        # Constant 1's mask value to re-use.
+        self._ones_mask = np.ones(window_size, dtype=int)
+
+    def load_data_offline(
+        self,
+        activity_coco: kwcoco.CocoDataset,
+        dets_coco: kwcoco.CocoDataset,
+        pose_coco: kwcoco.CocoDataset,
+        target_framerate: float,  # probably 15
+        framerate_round_decimals: int = 1,
+    ) -> None:
         """
-        Initializes the dataset.
+        Load data from filesystem resources for use during training.
+        """
+        # The data coverage for all the input datasets must be congruent.
+        logger.info("Checking dataset video/image congruency")
+        assert activity_coco.index.videos == dets_coco.index.videos  # noqa
+        assert activity_coco.index.videos == pose_coco.index.videos  # noqa
+        assert activity_coco.index.imgs == dets_coco.index.imgs
+        assert activity_coco.index.imgs == pose_coco.index.imgs
+
+        # Check that videos are, or are an integer multiple of, the target
+        # framerate.
+        logger.info("Checking video framerate multiples")
+        misaligned_fr = False
+        vid_id_to_fr_multiple: Dict[int, int] = {}
+        for vid_id, vid_obj in activity_coco.index.videos.items():
+            vid_fr: float = round(vid_obj["framerate"], framerate_round_decimals)
+            vid_fr_multiple = round(vid_fr / target_framerate, framerate_round_decimals)
+            if vid_fr_multiple.is_integer():
+                vid_id_to_fr_multiple[vid_id] = int(vid_fr_multiple)
+            else:
+                logger.error(
+                    f"Video ({vid_obj['name']}) framerate ({vid_fr}) is not a "
+                    f"integer multiple of the target framerate "
+                    f"({target_framerate}). Multiple found to be "
+                    f"{vid_fr_multiple}."
+                )
+                misaligned_fr = True
+        if misaligned_fr:
+            raise ValueError(
+                "Videos in the input dataset do not all align to the target "
+                "framerate. See error logging."
+            )
+
+        # Introspect from COCO categories pose keypoint counts.
+        # Later functionality assumes that all poses considered consist of the
+        # same quantity of keypoints, i.e. we cannot support multiple pose
+        # categories with **varying** keypoint quantities.
+        pose_num_keypoints_set: Set[int] = {
+            len(obj["keypoints"])
+            for obj in pose_coco.categories().objs
+            if "keypoints" in obj
+        }
+        if len(pose_num_keypoints_set) != 1:
+            raise ValueError(
+                "Input pose categories either did not contain any keypoints, "
+                "or contained multiple keypoint-containing categories with "
+                "varying quantities of keypoints. "
+                f"Found sizes in the set: {pose_num_keypoints_set}"
+            )
+        num_pose_keypoints = list(pose_num_keypoints_set)[0]
+
+        # For each video in dataset, collect "windows" of window_size entries.
+        # Each entry needs to be a collection of:
+        # * object detections on that frame
+        # * pose predictions (including keypoints) for that frame
+        #
+        # Videos that are a multiple of the target framerate should be treated
+        # as multiple videos, with sub-video is one frame offset from the
+        # others. E.g. 15Hz target but 30Hz video, sub-video 1 starts at frame
+        # 0 and taking every-other frame (30/15=2), sub-video 2 starts at frame
+        # 1 and taking every-other frame, thus each video considers unique
+        # frame products.
+
+        # Default "empty" instances to share for frames with no detections or
+        # poses. These are for storing empty, but shaped, arrays.
+        empty_dets = FrameObjectDetections(
+            np.ndarray(shape=(0, 4)),
+            np.ndarray(shape=(0,)),
+            np.ndarray(shape=(0,)),
+        )
+        empty_pose = FramePoses(
+            np.ndarray(shape=(0,)),
+            np.ndarray(shape=(0, num_pose_keypoints, 2)),
+            np.ndarray(shape=(0, num_pose_keypoints)),
+        )
+
+        # Collect per-frame data first per-video, then slice into windows.
+        #
+        # Windows of per-frame data that would go into producing a vector.
+        window_data: List[List[FrameData]] = []
+        # Activity classification truth labels per-frame per-window.
+        window_truth: List[List[int]] = []
+        # Video ID represented per window. Only one video should be represented
+        # in any one window.
+        window_vid: List[int] = []
+        # Image ID per-frame per-window.
+        window_gids: List[List[int]] = []
+        # cache frequently called module functions
+        np_asarray = np.asarray
+        for vid_id in tqdm(activity_coco.videos()):
+            vid_img_ids = list(activity_coco.images(video_id=vid_id))
+            # Iterate over sub-videos if applicable
+            vid_fr_multiple = vid_id_to_fr_multiple[vid_id]
+            for starting_idx in range(vid_fr_multiple):  # may just be a single [0]
+                # video-local storage to keep things separate, will extend main
+                # structures afterward.
+                vid_frame_truth = []
+                vid_frame_data = []
+                vid_gid = vid_img_ids[starting_idx::vid_fr_multiple]
+                for img_id in tqdm(vid_gid):
+                    img_act = activity_coco.annots(image_id=img_id)
+                    img_dets = dets_coco.annots(image_id=img_id)
+                    img_poses = pose_coco.annots(image_id=img_id)
+
+                    # There should only be one activity truth annotation per
+                    # image.
+                    assert (
+                        len(img_act) == 1
+                    ), "Only a single activity label is allowed per image."
+                    vid_frame_truth.append(img_act.cids[0])
+
+                    # There may be no detections on this frame.
+                    if img_dets:
+                        frame_dets = FrameObjectDetections(
+                            img_dets.boxes.data,
+                            np_asarray(img_dets.cids, dtype=int),
+                            np_asarray(img_dets.lookup("score"), dtype=float),
+                        )
+                    else:
+                        frame_dets = empty_dets
+
+                    # Only consider annotations that actually have keypoints.
+                    # There may be no poses on this frame.
+                    img_poses = img_poses.take(
+                        [
+                            idx
+                            for idx, obj in enumerate(img_poses.objs)
+                            if "keypoints" in obj
+                        ]
+                    )
+                    if img_poses:
+                        # Only keep the positions, drop visibility value.
+                        kp_pos = np_asarray(img_poses.lookup("keypoints")).reshape(
+                            -1, num_pose_keypoints, 3
+                        )[:, :, :2]
+                        frame_poses = FramePoses(
+                            np_asarray(img_poses.lookup("score"), dtype=float),
+                            kp_pos,
+                            np_asarray(
+                                img_poses.lookup("keypoint_scores"), dtype=float
+                            ),
+                        )
+                    else:
+                        frame_poses = empty_pose
+                    vid_frame_data.append(FrameData(frame_dets, frame_poses))
+
+                # Slide this video's worth of frame data into windows such that
+                # each window is window_size long.
+                # If this video has fewer frames than window_size, this video
+                # effectively be skipped.
+                vid_window_truth = []
+                vid_window_data = []
+                vid_window_gids = []  # Image IDs for frames of this window
+                for i in range(len(vid_frame_data) - self.window_size):
+                    vid_window_truth.append(vid_frame_truth[i : i + self.window_size])
+                    vid_window_data.append(vid_frame_data[i : i + self.window_size])
+                    vid_window_gids.append(vid_gid[i : i + self.window_size])
+
+                window_truth.extend(vid_window_truth)
+                window_data.extend(vid_window_data)
+                window_vid.append(vid_id)
+                window_gids.extend(vid_window_gids)
+
+        self._window_data = window_data
+        logger.info("Converting window content into arrays...")
+        self._window_truth = window_truth
+        self._window_vid = window_vid
+        self._window_gids = window_gids
+        logger.info("Converting window content into arrays... Done")
+
+    def load_data_online(
+        self,
+        window_detections: Sequence[FrameObjectDetections],
+        window_poses: Sequence[FramePoses],
+    ) -> None:
+        """
+        Receive data from a streaming runtime to yield from __getitem__.
+
+        If any one frame has no object detections or poses estimated for it,
+        `None` should be filled in the corresponding position(s).
 
         Args:
-            kwcoco_path: The source of data (can be real-time or offline).
-            sample_rate:
-            window_size:
+            window_detections: TODO
+            window_poses: TODO
         """
-        self.sample_rate = sample_rate
-        self.window_size = window_size
+        # Just load one windows worth of stuff so only __getitem__(0) makes
+        # sense.
+        if len(window_detections) != len(window_poses):
+            raise ValueError(
+                f"Input detections and poses sequences were of incongruent "
+                f"length ({len(window_detections)} != {len(window_poses)})."
+            )
+        if len(window_detections) != self.window_size:
+            raise ValueError(
+                f"Input sequences did not match the configured window size "
+                f"({len(window_detections)} != {self.window_size})."
+            )
 
-        self.dset = kwcoco.CocoDataset(kwcoco_path)
+        # Take in a vector of FrameObjectDetections and FramePoses directly.
+        # TODO: Check that the quantity input is each window_size in length.
+        # Form a single window, set to the
+        window_data = []
+        for fd, fp in zip(window_detections, window_poses):
+            window_data.append(FrameData(fd, fp))
+        self._window_data = [window_data]
+        # The following are undefined for online mode, so we're just filling in
+        # 0's enough to match size/shape requirements.
+        self._window_truth = [[0] * self.window_size]
+        self._window_vid = [0]
+        self._window_gids = [list(range(self.window_size))]
 
-        # for offline training, pre-cut videos into clips according to window size for easy batching
-        self.frames = []
-
-        vid_list = list(self.dset.index.videos.keys())
-        logger.info(
-            f"Generating dataset with {len(vid_list)} videos"
-        )
-        pber = tqdm(
-            vid_list,
-            total=len(vid_list),
-        )
-        for vid in pber:
-            video_dict = self.dset.index.videos[vid]
-
-            vid_frames = self.dset.index.vidid_to_gids[vid]
-
-            for index in range(0, len(vid_frames) - window_size - 1, sample_rate):
-                video_slice = vid_frames[index : index + window_size]
-                window_frame_dicts = [self.dset.index.imgs[gid] for gid in video_slice]
-
-                # start_frame = window_frame_dicts[0]['frame_index']
-                # end_frame = window_frame_dicts[-1]['frame_index']
-
-                # n_frames = end_frame - start_frame + 1
-
-                self.frames.append(window_frame_dicts)
-
-    def __getitem__(self, index):
+    def __getitem__(
+        self, index: int
+    ) -> Tuple[
+        npt.NDArray[float],
+        npt.NDArray[int],
+        npt.NDArray[int],
+        npt.NDArray[int],
+        npt.NDArray[int],
+    ]:
         """
         Fetches the data point and defines its TCN vector.
 
@@ -105,16 +357,35 @@ class TCNDataset(Dataset):
             index: The index of the data point.
 
         Returns:
-            tcn_vector: The TCN vector for the given index.
+            Series of 5 numpy arrays:
+
+              * Embedding Vector, shape: (window_size, n_dims)
+              * per-frame truth, shape: (window_size,)
+              * per-frame applicability mask, shape: (window_size,)
+              * per-frame video ID, shape: (window_size,)
+              * per-frame image ID, shape: (window_size,)
         """
+        window_data = self._window_data[index]
+        window_truth = self._window_truth[index]
+        window_vid = self._window_vid[index]
+        window_gids = self._window_gids[index]
 
-        data = self.frames[index]
+        # Under the current operation of this dataset, the mask should always
+        # consist of 1's. This may be removed in the future.
 
-        inputs = collect_inputs([self.dset, data], offline=True)
+        tcn_vector = vectorize(inputs)
+        if self.transform is not None:
+            tcn_vector = self.transform(tcn_vector)
 
-        # tcn_vector = define_tcn_vector(inputs)
-
-        return
+        return (
+            tcn_vector,
+            np.asarray(window_truth, dtype=int),
+            # Under the current operation of this dataset, the mask should always
+            # consist of 1's. This may be removed in the future.
+            self._ones_mask,
+            np.repeat(window_vid, self.window_size),
+            np.asarray(window_gids, dtype=int),
+        )
 
     def __len__(self):
         """
@@ -127,11 +398,31 @@ class TCNDataset(Dataset):
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+
     # Example usage:
-    kwcoco_path = "/data/PTG/medical/training/yolo_object_detector/detect/r18_all/r18_all_all_obj_results_with_dets_and_pose.mscoco.json"
+    activity_coco = kwcoco.CocoDataset(
+        # "/home/local/KHQ/paul.tunison/data/darpa-ptg/tcn_training_example/TEST-activity_truth.coco.json"
+        "/home/local/KHQ/paul.tunison/data/darpa-ptg/tcn_training_example/activity_truth.coco.json"
+    )
+    dets_coco = kwcoco.CocoDataset(
+        # "/home/local/KHQ/paul.tunison/data/darpa-ptg/tcn_training_example/TEST-object_detections.coco.json"
+        "/home/local/KHQ/paul.tunison/data/darpa-ptg/tcn_training_example/all_object_detections.coco.json"
+    )
+    pose_coco = kwcoco.CocoDataset(
+        # "/home/local/KHQ/paul.tunison/data/darpa-ptg/tcn_training_example/TEST-pose_estimates.coco.json"
+        "/home/local/KHQ/paul.tunison/data/darpa-ptg/tcn_training_example/all_poses.coco.json"
+    )
 
-    dataset = TCNDataset(kwcoco_path=kwcoco_path, sample_rate=1, window_size=25)
+    dataset = TCNDataset(window_size=25, feature_version=6)
+    dataset.load_data_offline(
+        activity_coco,
+        dets_coco,
+        pose_coco,
+        target_framerate=15,
+    )
 
+    breakpoint()
     print(f"dataset: {len(dataset)}")
     data_loader = torch.utils.data.DataLoader(dataset, batch_size=4, shuffle=True)
 
---
+++ tcn_hpl/data/vectorize.py
@@ -0,0 +1,81 @@
+"""
+Logic and utilities to perform the vectorization of input data into an
+embedding space used for TCN training and prediction.
+"""
+from dataclasses import dataclass
+from typing import Optional
+
+import numpy.typing as npt
+
+
+@dataclass
+class FrameObjectDetections:
+    """
+    Representation of object detection predictions for a single image.
+    All sequences should be the same length.
+    """
+
+    # Detection 2D bounding boxes in xywh format for the left, width, height and
+    # bottom coordinates respectively. Shape: (num_detections, 4)
+    boxes: npt.NDArray[float]
+    # Object category ID of the most confident class. Shape: (num_detections,)
+    labels: npt.NDArray[int]
+    # Vectorized detection confidence value of the most confidence class.
+    # Shape: (num_detections,)
+    scores: npt.NDArray[float]
+
+    def __post_init__(self):
+        assert self.boxes.ndim == 2
+        assert self.labels.ndim == 1
+        assert self.scores.ndim == 1
+        assert self.boxes.shape[0] == self.labels.shape[0]
+        assert self.boxes.shape[0] == self.scores.shape[0]
+        assert self.boxes.shape[1] == 4
+
+    def __bool__(self):
+        return bool(self.boxes.size)
+
+
+@dataclass
+class FramePoses:
+    """
+    Represents pose estimations for a single image.
+
+    We currently assume that all poses will be composed of the same number of
+    keypoints.
+    """
+
+    # Array of scores for each pose. Ostensibly the bbox score. Shape: (num_poses,)
+    score: npt.NDArray[float]
+    # Pose join 2D positions in ascending joint ID order. If the joint is not
+    # present, 0s are used. Shape: (num_poses, num_joints, 2)
+    joint_positions: npt.NDArray[float]
+    # Poise joint scores. Shape: (num_poses, num_joints)
+    joint_scores: npt.NDArray[float]
+
+    def __post_init__(self):
+        assert self.score.ndim == 1
+        assert self.joint_positions.ndim == 3
+        assert self.joint_scores.ndim == 2
+        assert self.score.shape[0] == self.joint_positions.shape[0]
+        assert self.score.shape[0] == self.joint_scores.shape[0]
+        assert self.joint_positions.shape[1] == self.joint_scores.shape[1]
+
+    def __bool__(self):
+        return bool(self.score.size)
+
+
+@dataclass
+class FrameData:
+    object_detections: Optional[FrameObjectDetections]
+    poses: Optional[FramePoses]
+
+    def __bool__(self):
+        return bool(self.object_detections) or bool(self.poses)
+
+
+
+def vectorize():
+    """
+    :return:
+    """
